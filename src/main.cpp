bf0c8dc271a09a6527518d1f0fbe140b9269137f
marcojonkhe1m <mjonkheim@gmail.com>
2025-11-30 11:10:13 +0100

    rough starting screen plus pokemon selection screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  std::cout << "\n===================================" << std::endl;
+  std::cout << "Welcome to Poke Damage Calculator!" << std::endl;
+  std::cout << "===================================" << std::endl;
+  std::cout << "\nChoose an option: " << std::endl;
+  std::cout << "\n1. Select Pokemon" << std::endl;
+  std::cout << "2. Exit" << std::endl;
+
+  int input;
+  std::cout << "\nEnter option: ";
+  std::cin >> input;
+
+  switch (input) {
+  case 1:
+    char *name;
+
+    std::cout << "\n==============================" << std::endl;
+    std::cout << "Pokemon selection screen" << std::endl;
+    std::cout << "==============================" << std::endl;
+    std::cout << "\nPlease enter Pokemon name: ";
+    std::cin >> name;
+    std::cout << "\n --> " << name << " selected by Player!\n" << std::endl;
+
+    std::cout << "\nPlease enter Opponent's Pokemon name: ";
+    std::cin >> name;
+    std::cout << "\n --> " << name << " selected by Opponent!\n" << std::endl;
+
+    break;
+
+  case 2:
+    std::cout << "\nExiting.......\n" << std::endl;
+    break;
+
+  default:
+    std::cout << "\nInput not valid...\nExiting.....\n" << std::endl;
+  }
 
   return 0;
 }
