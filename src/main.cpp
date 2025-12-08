cf5c097d9adf11169aa234a54200ab2a4fbbe52f
marcojonkhe1m <mjonkheim@gmail.com>
2025-12-08 21:15:37 +0100

    added player and enemy pokemon screens

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ typedef struct {
 
 int main() {
   win header;
+  win my_pokemon_window;
+  win enemy_pokemon_window;
 
   initscr();
   cbreak();
@@ -19,6 +21,17 @@ int main() {
   header.width = COLS;
   header.xstart = 0;
   header.ystart = 0;
+
+  my_pokemon_window.height = LINES - header.height;
+  my_pokemon_window.width = COLS / 2;
+  my_pokemon_window.xstart = 0;
+  my_pokemon_window.ystart = header.height;
+
+  enemy_pokemon_window.height = LINES - header.height;
+  enemy_pokemon_window.width = COLS / 2;
+  enemy_pokemon_window.xstart = COLS / 2;
+  enemy_pokemon_window.ystart = header.height;
+
   refresh();
 
   header.window =
@@ -28,8 +41,28 @@ int main() {
   mvwprintw(header.window, header.height / 2, header.width / 2, "Storm Silver");
   wrefresh(header.window);
 
+  my_pokemon_window.window =
+      newwin(my_pokemon_window.height, my_pokemon_window.width,
+             my_pokemon_window.ystart, my_pokemon_window.xstart);
+  box(my_pokemon_window.window, 0, 0);
+  mvwprintw(my_pokemon_window.window, 0, 3, "Player Pokemon");
+  mvwprintw(my_pokemon_window.window, my_pokemon_window.height / 2,
+            my_pokemon_window.width / 2, "Bulbasaur lv. 100");
+  wrefresh(my_pokemon_window.window);
+
+  enemy_pokemon_window.window =
+      newwin(enemy_pokemon_window.height, enemy_pokemon_window.width,
+             enemy_pokemon_window.ystart, enemy_pokemon_window.xstart);
+  box(enemy_pokemon_window.window, 0, 0);
+  mvwprintw(enemy_pokemon_window.window, 0, 3, "Enemy Pokemon");
+  mvwprintw(enemy_pokemon_window.window, my_pokemon_window.height / 2,
+            enemy_pokemon_window.width / 2, "Charmander lv. 100");
+  wrefresh(enemy_pokemon_window.window);
+
   getch();
   delwin(header.window);
+  delwin(my_pokemon_window.window);
+  delwin(my_pokemon_window.window);
   endwin();
   return 0;
 }
