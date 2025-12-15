0d1dbb12bc7bb55ef75521769784492e75b1c06c
marcojonkhe1m <mjonkheim@gmail.com>
2025-12-15 20:41:46 +0100

    added key events

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <string.h>
 
 // TODO (marco): wil ik de ncurses window * in de struct zetten?
 struct Window {
@@ -6,15 +7,34 @@ struct Window {
     int width;
     int starty;
     int startx;
+    const char* title;
 };
 
+WINDOW* create_bordered_window(struct Window* win_struct) {
+    WINDOW* win = newwin(win_struct->height, win_struct->width, win_struct->starty, win_struct->startx);
+    box(win, 0, 0);
+
+    if (win_struct->title != NULL) {
+        mvwprintw(win, 0, 2, "%s", win_struct->title);
+    }
+    wrefresh(win);
+
+    return win;
+}
+
+void destroy_window(WINDOW* win) {
+    delwin(win);
+}
+
 int main() {
+    int key;
     Window win = {};
 
     win.height = LINES;
     win.width = COLS;
     win.starty = 0;
     win.startx = 0;
+    win.title = " Pokemon Damage Calculator v0.1 ";
 
     initscr();
     cbreak();
@@ -22,13 +42,45 @@ int main() {
 
     refresh();
 
-    WINDOW* localWindow = newwin(win.height, win.width, win.starty, win.startx);
-    box(localWindow, 0, 0);
-    wrefresh(localWindow);
+    WINDOW* main_win = create_bordered_window(&win);
+
+    while (1) {
+        key = wgetch(main_win);
+        const char* text;
+
+        switch (key) {
+        case KEY_UP:
+            text = "arrow up pressed";
+            mvwprintw(main_win, LINES / 2, (COLS - strlen(text)) / 2, "%s", text);
+            wrefresh(main_win);
+            break;
+        case KEY_DOWN:
+            text = "arrow down pressed";
+            mvwprintw(main_win, LINES / 2, (COLS - strlen(text)) / 2, "%s", text);
+            wrefresh(main_win);
+            break;
+        case KEY_LEFT:
+            text = "arrow left pressed";
+            mvwprintw(main_win, LINES / 2, (COLS - strlen(text)) / 2, "%s", text);
+            wrefresh(main_win);
+            break;
+        case KEY_RIGHT:
+            text = "arrow right pressed";
+            mvwprintw(main_win, LINES / 2, (COLS - strlen(text)) / 2, "%s", text);
+            wrefresh(main_win);
+            break;
+        default:
+            text = "press an arrow key";
+            mvwprintw(main_win, LINES / 2, (COLS - strlen(text)) / 2, "%s", text);
+            wrefresh(main_win);
+            break;
+        }
+        break;
+    }
 
     getch();
 
-    delwin(localWindow);
+    destroy_window(main_win);
 
     return 0;
 }
