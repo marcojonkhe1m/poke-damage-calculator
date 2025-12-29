c877fd619813d483095a40d8182fc72b81c0eae0
marcojonkhe1m <mjonkheim@gmail.com>
2025-12-29 17:02:55 +0100

    expanded filehandler with gettotalbytes function

--- include/file_handler.h
+++ include/file_handler.h
@@ -1,2 +1,8 @@
+#pragma once
+
+#include <stddef.h>
+
 void *ReadEntireFile(const char *Filename);
 void FreeEntireFile(void *FileHandle);
+
+size_t GetTotalBytesInFile(int FileHandle);
--- src/file_handler.cpp
+++ src/file_handler.cpp
@@ -1,8 +1,43 @@
 #include "file_handler.h"
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 void *ReadEntireFile(const char *Filename) {
     void *Result = 0;
+    int FileHandle = 0;
+    size_t BytesInFile = 0;
+    void *Memory = 0;
+    ssize_t BytesRead = 0;
 
-    int FileHandle = open(Filename, O_RDONLY);
-};
+    FileHandle = open(Filename, O_RDONLY);
+
+    if (FileHandle != -1) {
+        BytesInFile = GetTotalBytesInFile(FileHandle);
+
+        if (BytesInFile != 0) {
+            BytesRead = read(FileHandle, Memory, BytesInFile);
+        }
+
+        else {
+            // TODO (marco): Logging
+        }
+    }
+
+    else {
+        // TODO (marco): Logging
+    }
+
+    return Result;
+}
+
+size_t GetTotalBytesInFile(int FileHandle) {
+    struct stat StatBuffer = {};
+    int Result = 0;
+    Result = fstat(FileHandle, &StatBuffer);
+
+    if (Result != -1) {
+        return (size_t)0;
+    }
+    return (size_t)StatBuffer.st_size;
+}
