6500bf41c2dcad945ffd746a906a4bca21095af4
marcojonkhe1m <mjonkheim@gmail.com>
2026-01-07 13:11:00 +0100

    new window

--- src/linux_pokecalc.cpp
+++ src/linux_pokecalc.cpp
@@ -1,13 +1,37 @@
 #include <ncurses.h>
 
+struct screen {
+    int Height;
+    int Width;
+    int StartX;
+    int StartY;
+    WINDOW *screen;
+};
+
 int main() {
+    screen MainScreen = {};
+
+    MainScreen.Height = LINES;
+    MainScreen.Width = COLS;
+    MainScreen.StartX = 0;
+    MainScreen.StartY = 0;
+
     initscr();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
 
-    getch();
+    MainScreen.screen = newwin(MainScreen.Height, MainScreen.Width, MainScreen.StartY, MainScreen.StartX);
+    if (MainScreen.screen) {
+        box(MainScreen.screen, 0, 0);
 
+        getch();
+
+        delwin(MainScreen.screen);
+    }
+    else {
+        // TODO: Logging
+    }
     endwin();
     return 0;
 }
