999282573785450c2c0a4dcb820806958b6604c1
marcojonkhe1m <mjonkheim@gmail.com>
2026-01-08 23:15:22 +0100

    added kernel signals resize and quit

--- src/linux_pokecalc.cpp
+++ src/linux_pokecalc.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
 #include <ncurses.h>
+#include <signal.h>
+
+static volatile sig_atomic_t Running = 1;
+static volatile sig_atomic_t ResizeRequested;
+
+static void SignalHandler(int Signo, siginfo_t *Info, void *Context) {
+    switch (Signo) {
+    case SIGWINCH:
+        ResizeRequested = 1;
+        break;
+    case SIGINT:
+    case SIGHUP:
+    case SIGTERM:
+        Running = 0;
+        break;
+    }
+}
 
 struct screen {
     int Height;
@@ -9,28 +27,52 @@ struct screen {
 };
 
 int main() {
-    screen MainScreen = {};
 
-    MainScreen.Height = LINES;
-    MainScreen.Width = COLS;
-    MainScreen.StartX = 0;
-    MainScreen.StartY = 0;
+    struct sigaction Sa = {};
+    Sa.sa_sigaction = SignalHandler;
+    Sa.sa_flags = SA_SIGINFO;
+
+    sigaction(SIGINT, &Sa, NULL);
+    sigaction(SIGTERM, &Sa, NULL);
+    sigaction(SIGHUP, &Sa, NULL);
+    sigaction(SIGWINCH, &Sa, NULL);
 
     initscr();
+    start_color();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
 
-    MainScreen.screen = newwin(MainScreen.Height, MainScreen.Width, MainScreen.StartY, MainScreen.StartX);
-    if (MainScreen.screen) {
-        box(MainScreen.screen, 0, 0);
+    while (Running) {
+        if (ResizeRequested) {
+            std::cerr << "Debug: SIGWINCH";
+        };
 
-        getch();
+        screen MainScreen = {};
 
-        delwin(MainScreen.screen);
-    }
-    else {
-        // TODO: Logging
+        MainScreen.Height = LINES;
+        MainScreen.Width = COLS;
+        MainScreen.StartX = 0;
+        MainScreen.StartY = 0;
+
+        init_pair(1, COLOR_WHITE, COLOR_BLACK);
+
+        MainScreen.screen = newwin(MainScreen.Height, MainScreen.Width, MainScreen.StartY, MainScreen.StartX);
+        if (MainScreen.screen != NULL) {
+            box(MainScreen.screen, 0, 0);
+            wrefresh(MainScreen.screen);
+
+            wattron(MainScreen.screen, COLOR_PAIR(1));
+            mvwprintw(MainScreen.screen, MainScreen.Height / 2, MainScreen.Width / 2, "HelloWorld!");
+            wrefresh(MainScreen.screen);
+
+            getch();
+            delwin(MainScreen.screen);
+        }
+
+        else {
+            // TODO: Logging
+        }
     }
     endwin();
     return 0;
